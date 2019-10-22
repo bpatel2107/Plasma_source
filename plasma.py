70c28b747e05f62431cd6d89d9be25e68c342093
Bhavin Patel <bhavin.patel@york.ac.uk>
2019-10-22 18:44:12 +0100

    Added in fusion power calculator and filtering function. Writing function to get co-ordinates out

--- plasma.py
+++ plasma.py
@@ -10,7 +10,7 @@ class Plasmas:
     """
 
     def __init__(self, Rmajor=2.5, aspect_ratio=1.67, elong=2.8, tri=0.0,
-                 T0=25.0, Tped_height=0.2, n0=16.0, nped_height=0.8,
+                 T0=25.0, Tped_height=0.2, n0=16.0, nped_height=0.8, ped_rad=0.9,
                  nrhos=20, ntheta=1000):
 
         # Radial zones
@@ -34,9 +34,14 @@ class Plasmas:
         self.n0 = n0
         self.nped_height = nped_height
 
+        self.ped_rad = ped_rad
+
         # Dataset containing all the grid
-        self.ds = self._create_grid(Rmajor=Rmajor, aspect_ratio=aspect_ratio, elong=elong, tri=tri,
-                                    T0=T0, Tped_height=Tped_height, n0=n0, nped_height=nped_height, rhos=self.rhos, theta=self.theta)
+        self.ds = self._create_grid(Rmajor=Rmajor, aspect_ratio=aspect_ratio,
+                                    elong=elong, tri=tri, T0=T0,
+                                    Tped_height=Tped_height, n0=n0,
+                                    nped_height=nped_height, ped_rad=ped_rad,
+                                    rhos=self.rhos, theta=self.theta)
 
         # Calculate area/volume of each plasma
         self.calc_plasma_vol()
@@ -69,4 +74,70 @@ class Plasmas:
         self.vol = vol
         self.dvol = dvol
 
-    # def calc_fusion_power(self):
+    def gen_kinetic_profiles(self):
+
+        ds = self.ds
+
+        T = ds['T0'] - ds['T0'] * \
+            (1 - ds['Tped_height'])/ds['ped_rad'] * ds['rhos']
+
+        n = ds['n0'] - ds['n0'] * \
+            (1 - ds['nped_height'])/ds['ped_rad'] * ds['rhos']
+
+        self.ds['T'] = T
+        self.ds['n'] = n
+
+    def calc_fusion_power(self, fuspow_type=0):
+
+        ds = self.ds
+        self.fuspow_type = fuspow_type
+
+        self.gen_kinetic_profiles()
+
+        if fuspow_type == 0:
+
+            pden = 1.1e-24 * ds['T']**2 * ds['n']**2 / \
+                4 * 17.5e6 * 1.6e-19 * self.dvol * 1e-9 * 1e38
+
+        else:
+
+            arg = -0.476 * np.abs(np.log(1.45e-5*ds['T']*1e3))**2.25
+
+            pden = 1.27e4 * ds['n']**2 * np.exp(arg) * self.vol * 1e-9
+
+        self.ds['pfus'] = pden.sum('rhos')
+
+    def filter_params(self, key=None, value=None, tol=None):
+
+        if key is None:
+            raise ValueError('Key needed to filter_params')
+
+        if value is None:
+            raise ValueError('Filter valued needed')
+
+        if tol is None:
+            raise ValueError('Tolerance to value must be set')
+
+        ds = self.ds[key]
+
+        filter_ds = ds.where(abs(ds.data - value) < tol)
+
+        return filter_ds
+
+
+def get_params(da=None):
+
+    if da is None:
+        raise ValueError('DataArray needed as input')
+
+    keys = da.to_dict()['coords'].keys()
+
+    filter_list = []
+    for pfus in da.data:
+
+        print(pfus)
+        # if not np.isnan(da.data):
+        # print(da)
+        # filter_list.append(da.coords)
+
+        # return filter_list
--- plasma_generator.py
+++ plasma_generator.py
@@ -1,80 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from plasma import Plasmas
+from plasma import Plasmas, get_params
 
 pi = np.pi
 
 Rmajor = np.array([2.5, 2.6])
-aspect_ratio = 1.61
+aspect_ratio = ([1.61, 1.7])
 
 aminor = Rmajor/aspect_ratio
 
-elong = 2.85
+elong = ([2.8, 2.9])
 
-tri = 0.0
+tri = ([0.0, 0.1])
 
 # Core temp in keV
-T0 = 25
+T0 = ([25.0, 22.0])
 
-Tped_height = 0.2
-
-Tped = T0 * Tped_height
+Tped_height = ([0.2, 0.3])
 
 # Density in core (#10^19)
-n0 = 16.0
+n0 = ([16.0, 18.0])
 
-nped_height = 0.8
+nped_height = ([0.8, 0.9])
 
-ped_rad = 0.9
+ped_rad = ([0.85, 0.9])
 
-nped = n0 * nped_height
-
-p0 = n0*T0 * 1.6
-
-nrhos = 20
-rho = np.linspace(0, 1, nrhos)
-rho_grid = np.linspace(0, 1, nrhos-1)
-rho = rho[:, np.newaxis]
+nrhos = 100
 
 ntheta = 1000
 
 pl = Plasmas(Rmajor, aspect_ratio, elong, tri, T0,
-             Tped_height, n0, nped_height, nrhos, ntheta)
+             Tped_height, n0, nped_height, ped_rad, nrhos, ntheta)
 
 
-T = T0 - (T0-Tped)/ped_rad * rho_grid
+pl.calc_fusion_power(fuspow_type=1)
+#print('SCENE calc = {} GW'.format(pl.ds['pfus'].data))
 
-n = n0 - (n0-nped)/ped_rad * rho_grid
+pl.calc_fusion_power()
+#print('Wesson calc = {} GW'.format(pl.ds['pfus'].data))
 
-theta = np.linspace(0, 2*pi, 1000)
-dtheta = theta[1] - theta[0]
+filter_da = pl.filter_params(key='pfus', value=1.0, tol=0.2)
 
-# Generate plasma boundary
-Rbnd = Rmajor + aminor * \
-    np.outer(rho, np.cos(theta + np.arcsin(tri)*np.sin(theta)))
-Zbnd = elong * aminor * np.outer(rho,  np.sin(theta))
-
-for i in range(nrhos):
-    plt.plot(Rbnd[i, :], Zbnd[i, :])
-plt.show()
-area = np.sum((Rbnd-Rmajor)**2 + Zbnd**2, axis=1) * dtheta / 2
-
-darea = np.diff(area)
-
-vol = 2 * pi * Rmajor * area
-dvol = 2 * pi * Rmajor * darea
-
-# arg = -0.476 * np.abs(np.log(1.45e-5*Tavg*1e3))**2.25
-
-# pden = 1.27e4 * navg**2 * np.exp(arg)
-
-# pfus = pden * vol
-
-# print('SCENE calc says {} GW'.format(pfus*1e-9))
-
-pfus_wes = 1.1e-24 * T**2 * \
-    (n*10**19)**2/4 * 17.5e6 * 1.6e-19 * dvol * 1e-9
-
-pfus = np.sum(pfus_wes)
-
-print('Wesson calc says {} GW'.format(pfus))
+filter_list = get_params(filter_da)
