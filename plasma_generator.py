9d3af20e73c8827af9e5f02b3e1b8b882e319739
Bhavin Patel <bhavin.patel@york.ac.uk>
2019-10-24 13:38:33 +0100

    Generates plasma for several parameters and now calculates the fusion power using 3 different methods

--- plasma_generator.py
+++ plasma_generator.py
@@ -44,34 +44,6 @@ alpha_n = ([1.0, 1.1])
 # Pedestal radial position
 ped_rad = ([0.85, 0.9])
 
-
-Rmajor = np.array([2.5, 2.6])
-aspect_ratio = 1.61
-
-aminor = Rmajor/aspect_ratio
-
-elong = 2.85
-
-tri = 0.0
-
-T0 = 25
-
-Tped_height = 0.2
-
-n0 = 14.0
-
-nped_height = 0.8
-
-ped_rad = 0.9
-
-alpha_T = 1.0
-
-alpha_n = 1.0
-
-nsep_height = 0.01
-Tsep_height = 0.01
-
-
 # No. of radial zones
 nrhos = 100
 
@@ -84,19 +56,6 @@ pl = Plasmas(Rmajor, aspect_ratio, elong, tri, T0,
              n0, nped_height, nsep_height, ped_rad,
              alpha_n, alpha_T, nrhos, ntheta)
 
-print('Area = {}'.format(pl.area.isel(rhos=-1).data))
-print('Vol  = {}'.format(pl.vol.isel(rhos=-1).data))
-
-# Calculate fusion powers of different scenarios
-pl.calc_fusion_power()
-print(pl.ds['pfus'].data)
-
-pl.calc_fusion_power(fuspow_type=1)
-print(pl.ds['pfus'].data)
-
-pl.calc_fusion_power(fuspow_type=2)
-print(pl.ds['pfus'].data)
-
 # Parameter to filter
 key = 'pfus'
 
@@ -106,16 +65,35 @@ value = 1.0
 # Tolerance of parameters
 tol = 0.2
 
-# Filter around given parameter
-filter_da = pl.filter_params(key='pfus', value=1.0, tol=0.2)
+#
+fustype = ['Fausser', 'Wesson', 'SCENE']
 
-filter_params = get_params(filter_da)
+# Loop through different ways of calculating fusion power
+for i in range(3):
+    print('!!! {} Method !!!'.format(fustype[i]))
 
-nparams = len(filter_params)
-names = filter_da.coords.to_index().names
+    # Calculate fusion powers of different scenarios
+    pl.calc_fusion_power(fuspow_type=i)
 
-# for i in range(len(filter_params)):
-#    print(filter_params[i])
-print('There are {} permutations that are are with {} of {} for {} '.format(
-    nparams, tol, value, key))
-print(names)
+    # Filter around given parameter
+    filter_da = pl.filter_params(key='pfus', value=1.0, tol=0.2)
+
+    # Get list of suitable parameters
+    filter_params = get_params(filter_da)
+
+    nparams = len(filter_params)
+    names = filter_da.coords.to_index().names
+
+    # for i in range(len(filter_params)):
+    #    print(filter_params[i])
+    print('There are {} permutations that are are with {} of {} for {} '.format(
+        nparams, tol, value, key))
+
+    # Get first value in form to be read by datarray
+    d = {name: par for name, par in zip(names, filter_params[0])}
+
+    for key, val in zip(d.keys(), d.values()):
+        print('{} = {}'.format(key, val))
+
+    print('{} method for fusion power {:.3f} GW\n'.format(
+        fustype[i], filter_da.sel(d).data))
